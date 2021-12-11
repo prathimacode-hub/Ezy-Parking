0142e73b0cffd4e5918f375f96e0785668fb6504
Rutik <71027989+Rutikab12@users.noreply.github.com>
2021-12-12 00:05:38 +0530

    Updated part

---
+++ js/extra.js
@@ -0,0 +1,26 @@
+/* Card Slider - Swiper 
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 3,
+	spaceBetween: 70,
+	breakpoints: {
+		// when window is <= 767px
+		767: {
+			slidesPerView: 1
+		},
+		// when window is <= 991px
+		991: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+*/
